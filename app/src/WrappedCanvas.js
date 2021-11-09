d84b5cc72cd73647544ce37173a9f0e2cfc1a276
MrTops <43430084+MrTops@users.noreply.github.com>
2021-11-09 12:47:29 -0500

    Create WrappedCanvas.js

---
+++ app/src/WrappedCanvas.js
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+
+const WrappedCanvas = props => {
+
+    const ref = useRef(null);
+
+    const cleanupFunction = props.cleanupFunction || function () {/* empty */};
+    const drawingFunction = props.drawingFunction || function () {/* empty */};
+
+    useEffect(() => {
+        cleanupFunction(ref.current.getContext("2d"));
+        drawingFunction(ref.current.getContext("2d"));
+    });
+
+    return (<canvas ref={ref} props={{...props}} />)
+};
+
+export default WrappedCanvas;
