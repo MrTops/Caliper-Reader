93015a5741dcd043d98bc71c40e28d72ce5775af
MrTops <43430084+MrTops@users.noreply.github.com>
2021-11-09 12:47:46 -0500

    Add test for caliper

--- app/src/Quiz.js
+++ app/src/Quiz.js
@@ -1,10 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Caliper from "./Caliper";
 import ContentWrapper from "./ContentWrapper";
 
 const Quiz = props => {
+	const [inputValue, setInputValue] = useState(0);
+
+	const handleValueChange = e => {
+		const newValue = e.target.value;
+		console.log(parseInt(newValue));
+		setInputValue(parseInt(newValue) || 0);
+	};
+
 	return (<ContentWrapper isActive={props.isActive}>
 		<div className="big-container">
-			<p>Quiz</p>
+			<Caliper value={inputValue} />
+			<form>
+				<label>
+					Answer:<br/>
+					<input type="number" value={inputValue} onChange={handleValueChange} />
+				</label>
+				<br/>
+				<input type="submit" value="Check Answer" />
+			</form>
 		</div>
 	</ContentWrapper>);
 };
